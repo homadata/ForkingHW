fdd31d59d2a17142b556906d97e567abcdfdc8d2
thezaro <98924064+thezaro@users.noreply.github.com>
2022-03-17 15:30:57 -0400

    Update ForkingHomeworkRfile.R

--- ForkingHomeworkRfile.R
+++ ForkingHomeworkRfile.R
@@ -10,3 +10,10 @@ diamonds%>%
   filter(price > 7000 & price < 10000)%>%
   arrange(desc(carat))%>%
   slice(1:20)
+m = as.data.table(diamonds)
+m [, . (carat,cut,price)]
+m = m [cut == "Premium"]
+m = m[price > 7000 & price < 10000 ]
+m = m [order(rank(-carat))]
+z = m  [1:20, . (carat,cut,price)   ] 
+z
